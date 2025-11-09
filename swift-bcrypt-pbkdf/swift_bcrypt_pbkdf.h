2687d31fe3a1b0fdc5d40a3e505db311f1631d87
Simon Bruce-Cassidy <simon@gale-huset.net>
2025-11-09 21:03:28 +0100

    Initial Commit

---
+++ swift-bcrypt-pbkdf/swift_bcrypt_pbkdf.h
@@ -0,0 +1,12 @@
+//
+//  swift_bcrypt_pbkdf.h
+//  swift-bcrypt-pbkdf
+//
+//  Created by Simon Bruce-Cassidy on 09/11/2025.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface swift_bcrypt_pbkdf : NSObject
+
+@end
